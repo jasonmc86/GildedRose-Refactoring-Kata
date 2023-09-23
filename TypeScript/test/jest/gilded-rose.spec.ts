c48b0d255958d25e8d95aa9bb5f18c1d8fd8f1ec
Jason McMonagle <jason.mcmonagle@gmail.com>
2023-09-23 21:40:15 +1200

    adds a couple of unit tests passing matching requirements, ready to refactor tests a bit

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,30 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
+  describe('updateQuality', () => {
+    describe('Given regular item', () => {
+      it('sellIn is lowered by 1', () => {
+        const originalSellIn = 5;
+        const gildedRose = new GildedRose([new Item('foo', originalSellIn, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toBe(originalSellIn - 1);
+      });
+
+      it('quality is lowered by 1', () => {
+        const originalQuality = 5;
+        const gildedRose = new GildedRose([new Item('foo', 5, originalQuality)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toBe(originalQuality - 1);
+      });
+
+      it('once sellIn date is negative, quality degrades twice as fast', () => {
+        const originalQuality = 5;
+        const sellIn = -1;
+        const gildedRose = new GildedRose([new Item('foo', sellIn, originalQuality)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toBe(originalQuality - 2);
+      });
+    })
+
+  })
 });
