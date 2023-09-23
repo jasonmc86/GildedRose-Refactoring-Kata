341ce02a6e5a3142158fc7dd1aece0bb6e4ebb45
Jason McMonagle <jason.mcmonagle@gmail.com>
2023-09-23 22:05:19 +1200

    adds unit tests for all original items

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -2,7 +2,7 @@ import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
   describe('updateQuality', () => {
-    describe('Given regular item', () => {
+    describe('Given any item', () => {
       it('sellIn is lowered by 1', () => {
         const originalSellIn = 5;
         const actualItem = setupAndRunGildedRoseWithItemUnderTest(originalSellIn)
@@ -23,6 +23,86 @@ describe('Gilded Rose', () => {
         const actualItem = setupAndRunGildedRoseWithItemUnderTest(sellIn, originalQuality)
 
         expect(actualItem.quality).toBe(originalQuality - 2);
+      });
+
+      it('when quality would otherwise be set to negative, set 0', () => {
+        const originalQuality = 1;
+        const sellIn = -1;
+        const actualItem = setupAndRunGildedRoseWithItemUnderTest(sellIn, originalQuality)
+
+        expect(actualItem.quality).toBe(0);
+      });
+
+      describe('Aged Brie special item', () => {
+        const itemName = 'Aged Brie';
+
+        it('when sellIn decreases, quality actually increases', () => {
+          const originalQuality = 5;
+          const actualItem = setupAndRunGildedRoseWithItemUnderTest(undefined, originalQuality, itemName)
+
+          expect(actualItem.quality).toBe(originalQuality + 1);
+        });
+
+        it('when quality increases, it never passes 50', () => {
+          const originalQuality = 50;
+          const actualItem = setupAndRunGildedRoseWithItemUnderTest(undefined, originalQuality, itemName)
+
+          expect(actualItem.quality).toBe(originalQuality);
+        });
+      });
+
+      describe('Sulfuras special item', () => {
+        const itemName = 'Sulfuras, Hand of Ragnaros';
+
+        it('sellIn never changes', () => {
+          const originalSellIn = 5;
+          const actualItem = setupAndRunGildedRoseWithItemUnderTest(originalSellIn, undefined, itemName)
+
+          expect(actualItem.sellIn).toBe(originalSellIn);
+        });
+
+        it('quality never changes', () => {
+          const originalQuality = 5;
+          const actualItem = setupAndRunGildedRoseWithItemUnderTest(undefined, originalQuality, itemName)
+
+          expect(actualItem.quality).toBe(originalQuality);
+        });
+      });
+
+      describe('Backstage passes special item', () => {
+        const itemName = 'Backstage passes to a TAFKAL80ETC concert';
+
+        it('when sellIn decreases, quality actually increases', () => {
+          const originalSellin = 20;
+          const originalQuality = 5;
+          const actualItem = setupAndRunGildedRoseWithItemUnderTest(originalSellin, originalQuality, itemName)
+
+          expect(actualItem.quality).toBe(originalQuality + 1);
+        });
+
+        it('when sellIn is equal or below 10 days, quality actually increases by 2', () => {
+          const originalSellin = 10;
+          const originalQuality = 5;
+          const actualItem = setupAndRunGildedRoseWithItemUnderTest(originalSellin, originalQuality, itemName)
+
+          expect(actualItem.quality).toBe(originalQuality + 2);
+        });
+
+        it('when sellIn is equal or below 5 days, quality actually increases by 3', () => {
+          const originalSellin = 5;
+          const originalQuality = 5;
+          const actualItem = setupAndRunGildedRoseWithItemUnderTest(originalSellin, originalQuality, itemName)
+
+          expect(actualItem.quality).toBe(originalQuality + 3);
+        });
+
+        it('when sellIn is below 0, quality sets to 0', () => {
+          const originalSellin = -1;
+          const originalQuality = 5;
+          const actualItem = setupAndRunGildedRoseWithItemUnderTest(originalSellin, originalQuality, itemName)
+
+          expect(actualItem.quality).toBe(0);
+        });
       });
 
       const setupAndRunGildedRoseWithItemUnderTest = (sellIn: number = 5, quality: number = 5, name: string = 'foo') => {
