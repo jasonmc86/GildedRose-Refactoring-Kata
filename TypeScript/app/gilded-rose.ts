7006f30b4a0d641e442ee4d996a8cecb54072a87
Jason McMonagle <jason.mcmonagle@gmail.com>
2023-09-25 20:39:14 +1300

    starting refactor - regular quality

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -12,19 +12,31 @@ export class Item {
 
 export class GildedRose {
   items: Array<Item>;
+  irregularItems = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros']
 
   constructor(items = [] as Array<Item>) {
     this.items = items;
   }
 
+  updateRegularQuality(item: Item) {
+    const multiplier = item.sellIn < 0 ? 2 : 1;
+
+    const updatedQuality = item.quality - 1 * multiplier;
+
+    item.quality = updatedQuality < 0 ? 0 : updatedQuality;
+  }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      const currentItem = this.items[i];
+      const isRegularItem = !this.irregularItems.includes(currentItem.name);
+      if (isRegularItem) {
+        this.updateRegularQuality(currentItem);
+      }
+
+
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
+
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
@@ -48,11 +60,7 @@ export class GildedRose {
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
+
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality
           }
